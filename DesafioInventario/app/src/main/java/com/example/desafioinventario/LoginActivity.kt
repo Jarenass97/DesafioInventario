e50bb30e0014b2aec2cce51373a1894febb051ae
Jorge Arenas <jorgearenass97@gmail.com>
2021-12-01 14:57:04 +0100

    implementado cambio correcto entre login y registro

--- DesafioInventario/app/src/main/java/com/example/desafioinventario/LoginActivity.kt
+++ DesafioInventario/app/src/main/java/com/example/desafioinventario/LoginActivity.kt
@@ -1,12 +1,54 @@
 package com.example.desafioinventario
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.TextView
+import android.widget.ViewAnimator
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var login_reg: ViewAnimator
+    var isLogin = true
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
+
+        login_reg = findViewById(R.id.vaLogReg)
+        val txtRegistrar = findViewById<TextView>(R.id.txtRegistrar)
+        txtRegistrar.setOnTouchListener(View.OnTouchListener { view, motionEvent ->
+            when (motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> view.setBackgroundResource(R.color.pulsacion)
+                MotionEvent.ACTION_UP -> {
+                    view.setBackgroundColor(Color.TRANSPARENT)
+                    login_reg.showNext()
+                    isLogin = false
+                }
+            }
+            true
+        })
+    }
+
+    fun iniciarSesion(view: View) {
+        //Buscar username en base de datos y comprobar si la contraseña es correcta. en caso afirmativo acceder
+        //al menú principal de la app
+    }
+
+    override fun onBackPressed() {
+        if (isLogin) super.onBackPressed()
+        else {
+            login_reg.showPrevious()
+            isLogin = true
+        }
+    }
+
+    fun registrar(view: View) {
+        login_reg.showPrevious()
+        isLogin = true
     }
 }
