3f028b369d3f8a7c394c37e5009aa19cfb2bbf7a
Jorge Arenas <jorgearenass97@gmail.com>
2021-12-05 11:58:21 +0100

    Implementado NavigationDrawer

--- DesafioInventario/app/src/main/java/com/example/desafioinventario/InterfazUsuarioActivity.kt
+++ DesafioInventario/app/src/main/java/com/example/desafioinventario/InterfazUsuarioActivity.kt
@@ -1,40 +1,57 @@
 package com.example.desafioinventario
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
-import model.Usuario
+import android.view.Menu
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.example.desafioinventario.databinding.ActivityInterfazUsuarioBinding
 
 class InterfazUsuarioActivity : AppCompatActivity() {
-    lateinit var usuario: Usuario
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityInterfazUsuarioBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_interfaz_usuario)
-        supportActionBar?.hide()
 
-        val bun: Bundle = intent.extras!!
-        usuario = bun.getSerializable("user") as Usuario
+        binding = ActivityInterfazUsuarioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setSupportActionBar(binding.appBarInterfazUsuario.toolbar)
+
+        binding.appBarInterfazUsuario.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_interfaz_usuario)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 
-    override fun onBackPressed() {
-        AlertDialog.Builder(this)
-            .setTitle(getString(R.string.strTituloCerrarSesion))
-            .setMessage(getString(R.string.strMensajeCerrarSesion))
-            .setPositiveButton("OK") { view, _ ->
-                val intent = Intent(this, LoginActivity::class.java)
-                startActivity(intent)
-                finish()
-                view.dismiss()
-            }
-            .setNegativeButton("NO") { view, _ ->
-                view.dismiss()
-            }
-            .setCancelable(true)
-            .create()
-            .show()
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.interfaz_usuario, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_interfaz_usuario)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
